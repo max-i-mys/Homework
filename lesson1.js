162e86b09cfeea7ef8163056e92a847d26f58a0f
max-i-mys <maximys6205@gmail.com>
2021-09-16 10:37:49 +0300

    moved the formation of a formula into a variable

--- lesson1.js
+++ lesson1.js
@@ -1,6 +1,18 @@
 const solveQuadraticEquation = array => {
 	const [a, b, c] = array
 	const discriminant = b ** 2 - 4 * a * c
+	const equation = `${a !== 1 && a !== -1 ? a : a < 0 ? "-" : ""}x^2${
+		b
+			? b !== 1 && b !== -1
+				? b < 0
+					? `${b}x`
+					: `+${b}x`
+				: b > 0
+				? "+x"
+				: "-x"
+			: ""
+	}${c ? (c < 0 ? c : `+${c}`) : ""}=0;`
+	const formulaX1 = (-b + Math.sqrt(discriminant)) / (2 * a)
 	switch (true) {
 		case a === 0: {
 			console.log(`Первое значение не может быть "0"`)
@@ -9,33 +21,21 @@ const solveQuadraticEquation = array => {
 		case discriminant === 0: {
 			const x1 = (-b + Math.sqrt(discriminant)) / (2 * a)
 			console.log(
-				`Уравнение: ${a !== 1 && a !== -1 ? a : a < 0 ? "-" : ""}x^2${
-					b !== 1 ? (b < 0 ? b : `+${b}`) : "+"
-				}x${
-					c ? (c < 0 ? c : `+${c}`) : ""
-				}=0; Дискриминант: ${discriminant}; корень: x = ${x1}`
+				`Уравнение: ${equation} Дискриминант: ${discriminant}; корень: x = ${x1}`
 			)
 			break
 		}
 		case discriminant > 0: {
-			const x1 = (-b + Math.sqrt(discriminant)) / (2 * a)
+			const x1 = formulaX1
 			const x2 = (-b - Math.sqrt(discriminant)) / (2 * a)
 			console.log(
-				`Уравнение: ${a !== 1 && a !== -1 ? a : a < 0 ? "-" : ""}x^2${
-					b !== 1 ? (b < 0 ? b : `+${b}`) : "+"
-				}x${
-					c ? (c < 0 ? c : `+${c}`) : ""
-				}=0; Дискриминант: ${discriminant}; корни: x1 = ${x1}, x2 = ${x2}`
+				`Уравнение: ${equation} Дискриминант: ${discriminant}; корни: x1 = ${x1}, x2 = ${x2}`
 			)
 			break
 		}
 		case discriminant < 0:
 			console.log(
-				`Уравнение: ${a !== 1 && a !== -1 ? a : a < 0 ? "-" : ""}x^2${
-					b !== 1 ? (b < 0 ? b : `+${b}`) : "+"
-				}x${
-					c ? (c < 0 ? c : `+${c}`) : ""
-				}=0; Дискриминант: ${discriminant}; корней нет`
+				`Уравнение: ${equation} Дискриминант: ${discriminant}; корней нет`
 			)
 			break
 		default:
